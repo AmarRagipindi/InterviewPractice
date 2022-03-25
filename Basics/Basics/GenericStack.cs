87dfd857d35597aed3ce09a0e7dcd2c9f09ede04
Amar RAGIPINDI <amaranatha.ragipindi@se.com>
2022-03-26 01:07:56 +0530

    Initial commit

---
+++ Basics/Basics/GenericStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class MyStack<T> where T :new()
+    {
+        public List<T> 
+        public static void Push<T>(T item)
+        {
+
+
+        }
+    }
+}
---
+++ Basics/Basics/PassingParameters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Basics
+{
+    class PassingParameters
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int valuetoSend = 10;
+            PassbyValue(valuetoSend);
+            Console.WriteLine($"Vaule is not retained {valuetoSend}");
+
+            PassbyReferenceRef(ref valuetoSend);
+            Console.WriteLine($"Vaule is not retained {valuetoSend}");
+
+            PassbyReferenceOut(out int uninitialized);
+            Console.WriteLine($"Vaule is not retained {uninitialized}");
+
+
+            PassbyReferenceIn(in valuetoSend);
+            Console.WriteLine($"Vaule is not retained {valuetoSend}");
+
+
+            Console.ReadLine();
+        }
+
+        static void PassbyValue(int a)
+        {
+            a = a + 10;
+        }
+
+        static void PassbyReferenceRef(ref int a)
+        {
+            a += 10;
+        }
+        static void PassbyReferenceOut(out int a)
+        {
+            a = 0;
+            a += 100;
+        }
+
+        static void PassbyReferenceIn(in int a)
+        {
+            //a = 0;
+            //a += 100;
+        }
+    }
+}
